5e339f8220c75c26e3cc0aa59a66253825459be8
Gage Henderson <gage@quangoinc.com>
2017-09-20 10:28:56 -0700

    Added Home page component

---
+++ killstreak/src/components/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../../css/Home.css';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="page Home"> 
+
+      </div>
+    );
+  }
+}
+
+export default Home;
--- killstreak/src/index.js
+++ killstreak/src/index.js
@@ -4,11 +4,13 @@ import { HashRouter, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
+import Home from './components/Pages/Home/Home';
 
 ReactDOM.render(
   <HashRouter>
     <div>
       <Route component={App} /> {/* Always render this */}
+      <Route exact path='/' component={Home} />
     </div>
   </HashRouter>,
   document.getElementById('root')
