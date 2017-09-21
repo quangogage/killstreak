a20a6e78979f7900704c741eef7a681496c9028f
Gage Henderson <gage@quangoinc.com>
2017-09-21 13:33:33 -0700

    More progress on shooting transition

--- killstreak/src/components/Pages/Transitions/1.js
+++ killstreak/src/components/Pages/Transitions/1.js
@@ -1,8 +1,9 @@
-/*
-  BANG BANG
-  BRRRRRRRRRRRAAAAHAP
-*/
+/* BANG BANG BRRRRRRRRRRRAAAAHAP */
 import $ from 'jquery';
+import gageMath from '../../../gagelib/Math';
+
+import decalImage from '../../../images/transitions/shooting/decal.png';
+
 
 // Create the red overlay element
 function createOverlay() {
@@ -13,21 +14,63 @@ function createOverlay() {
     top:0,
     width:'100%',
     height:'100%',
-    zIndex:5,
+    zIndex:2,
     background:'RGB(255,0,0)',
     opacity:0
   })
-  overlay.animate({
-    opacity:1
-  },500);
+  
   $('body').append(overlay);
   return overlay;
+}
+
+// Add a decal
+function createDecal(overlay) {
+  var decal=$(`<img class='decal' src=${decalImage} />`);
+  var x=gageMath.getRandom(0,window.innerWidth);
+  var y=gageMath.getRandom(0,window.innerHeight);
+  var size=gageMath.getRandom(15,42);
+  var rotation=gageMath.getRandom(0,360);
+
+  // Position and style
+  decal.css({
+    position:'absolute',
+    left:x+'px',
+    top:y+'px',
+    width:size+'px',
+    height:size+'px',
+    zIndex:3,
+    transform:`rotate(${rotation}deg)`
+  })
+
+  // Add to overlay
+  overlay.append(decal);  
+}
+
+// Begin shooting
+function beginShooting(overlay) {
+  var shooting=setInterval(function() {
+    createDecal(overlay);
+    if ($(".overlay .decal").length>=5) {
+      clearInterval(shooting);
+    }
+  },250)    
 }
 
 
 // Master Function \\
 function animation(newPage) {  
   var overlay=createOverlay();
+
+  // Fade overlay in
+  overlay.animate({
+    opacity:1
+  },500, function() {
+
+    // BRRRrrrrRRrrrrrrrrrrrRRRRRAAAAAP🔫
+    setTimeout(function() {    
+      beginShooting(overlay);
+    },400)
+  });
 }
 
 export default { animation }
