d85188e474769e430621003fb66304cd408192ed
Gage Henderson <gage@quangoinc.com>
2017-09-22 16:59:54 -0700

    Made napo the content font, added line breaks to make copy seem less overwhelming -matt ox

--- killstreak/src/components/Pages/About/About.js
+++ killstreak/src/components/Pages/About/About.js
@@ -23,12 +23,12 @@ class About extends Component {
 
           I started Killstreak to help create a platform
           for underground artists such as myself to be
-          heard and have a voice.
-
+          heard and have a voice.<br />
+          <br />
           We are an independently owned brand based out
           of the pacific northwest providing a versatile
-          set of products.
-
+          set of products.<br />
+          <br />
           By incorporating every day life into my work,
           I try to focus on capturing the smaller things
           in life that people tend to pass up by bringing
