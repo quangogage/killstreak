684cec4c864051806b52d323d390a14d583decf6
Gage Henderson <gage@quangoinc.com>
2017-09-22 14:57:58 -0700

    Added wave

--- killstreak/src/components/Pages/About/Header.js
+++ killstreak/src/components/Pages/About/Header.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import '../../../styles/css/About/Header.css';
 
 
+var waveDensity=25; // How many bars in the wave?
+var waveDelay=0.1; // Delay between each bars wave animation.
+var waveTime='2.25s' // Time for each bar to go up and down once
+
 class Header extends Component {
+
+  componentDidMount() {
+    this.createWave();
+  }
+
+  //Generate the wave elements
+  createWave() {
+    var container=$('.wave-container');
+    for(var i=0;i<waveDensity;i++) {
+      var bar=$('<div class="bar"></div>');
+      bar.css({
+        width:(window.innerWidth/waveDensity)+'px',
+        animationDelay:i*waveDelay+'s',
+        animationDuration:waveTime
+      })
+      container.append(bar);
+    }
+  }
+
+
   render() {
     return (
       <div className="Header">
+
+        <div className='wave-container'>
+        </div>
 
         {/* Title */}
         <div className='title-container'>         
