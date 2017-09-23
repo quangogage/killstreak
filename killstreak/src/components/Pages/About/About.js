537273355de6ea08b7091da8f7fc00c681c766e6
quangogage <quangogage@users.noreply.github.com>
2017-09-23 00:44:13 -0700

    Added content to about page

--- killstreak/src/components/Pages/About/About.js
+++ killstreak/src/components/Pages/About/About.js
@@ -13,7 +13,8 @@ class About extends Component {
     }
 
     return (
-      <div className="About">                 
+      <div className="About">      
+
         {/* Header */}
         <div className='header'>
 
@@ -23,6 +24,26 @@ class About extends Component {
           <div className='title-container'>
             <img src={logo} alt='Killstreak' />
           </div>          
+
+        </div>
+
+        {/* Content */}
+        <div className='content'>
+
+          I started Killstreak to help create a platform
+          for underground artsits such as myself to be
+          heard and have a voice. <br />
+          <br />
+
+          We are an independently owned brand based out
+          of the pacific northwest providing a versatile
+          set of products.<br />
+          <br />
+
+          By incorporating a lot of everyday life into
+          my work, I try to focus on capturing the smaller
+          things in life that people tend to pass up by 
+          bringing them into the bigger picture.        
 
         </div>
 
