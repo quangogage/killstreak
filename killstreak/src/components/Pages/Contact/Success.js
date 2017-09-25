96e47d95d396a2db97d32994d4efd3a52b319f22
Gage Henderson <gage@quangoinc.com>
2017-09-25 13:32:25 -0700

    Success message prompt styles

--- killstreak/src/components/Pages/Contact/Success.js
+++ killstreak/src/components/Pages/Contact/Success.js
@@ -4,8 +4,15 @@ import '../../../styles/css/Contact/Success.css';
 class Success extends Component {
   render() {
     return (
-      <div className="Success">
-        asdf
+      <div className={"Success" + (this.props.isOpen ? "" : " hidden")}>
+
+        <div className='content'>
+          <div className='die'>ú</div>
+          <div className='text'>Your message has been sent.</div>
+
+          <div className='button'>Okay</div>
+        </div>
+
       </div>
     );
   }
