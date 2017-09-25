d6b78d811364a192dcd27c12136cb7a10220458e
quangogage <quangogage@users.noreply.github.com>
2017-09-24 17:37:31 -0700

    Created gallery page and stylesheet

---
+++ killstreak/src/components/Pages/Gallery/Gallery.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../../styles/css/Gallery/Gallery.css';
+
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="Gallery page">
+      </div>
+    );
+  }
+}
+
+export default Gallery;
