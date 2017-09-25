a23963bf607eac93527ba9ee6a30160333ec034a
quangogage <quangogage@users.noreply.github.com>
2017-09-24 17:38:41 -0700

    Added conditional rendering to gallery page

--- killstreak/src/components/Pages/Gallery/Gallery.js
+++ killstreak/src/components/Pages/Gallery/Gallery.js
@@ -4,6 +4,13 @@ import '../../../styles/css/Gallery/Gallery.css';
 
 class Gallery extends Component {
   render() {
+
+    // Don't render if this page isn't
+    // the current one.
+    if (this.props.currentPage!=="Gallery") {
+      return false;
+    }
+
     return (
       <div className="Gallery page">
       </div>
