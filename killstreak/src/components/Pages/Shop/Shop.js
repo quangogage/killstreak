8e03accaabdb118b02bc2ee02857203ae0686f40
Gage Henderson <gage@quangoinc.com>
2017-09-29 12:49:14 -0700

    Added shop header component

--- killstreak/src/components/Pages/Shop/Shop.js
+++ killstreak/src/components/Pages/Shop/Shop.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import '../../../styles/css/Shop/Shop.css';
+import React, { Component } from "react";
+import "../../../styles/css/Shop/Shop.css";
+
+import Header from "./Header";
 
 class Shop extends Component {
   render() {
     return (
       <div className="Shop">
-
-        
-
+        <Header />
       </div>
     );
   }
