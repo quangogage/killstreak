92517797aaed26b820ee353fd699eb94c56d1d5f
Gage Henderson <gage@quangoinc.com>
2017-09-29 13:18:55 -0700

    Added nav to shop

--- killstreak/src/components/Pages/Shop/Shop.js
+++ killstreak/src/components/Pages/Shop/Shop.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../../../styles/css/Shop/Shop.css";
 
+import Nav from "../Nav/Nav";
 import Header from "./Header";
 
 class Shop extends Component {
   render() {
     return (
       <div className="Shop">
+        <Nav pages={this.props.pages} currentPage={this.props.currentPage} />
         <Header />
       </div>
     );
