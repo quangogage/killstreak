240b84a9754514af78f697975f6532a1a97097f1
Gage Henderson <gage@quangoinc.com>
2017-09-29 14:09:33 -0700

    Added product list file containing ( and exporting ) all product data

---
+++ killstreak/src/components/Pages/Shop/Products/list.js
@@ -0,0 +1,98 @@
+var list = [
+  /**********/
+  /* Shirts */
+  /**********/
+  {
+    name: "basic white",
+    images: [
+      "/shirts/basic white/1.jpg",
+      "/shirts/basic white/2.jpg",
+      "/shirts/basic white/3.jpg"
+    ],
+    tags: ["shirt", "white"]
+  },
+  {
+    name: "white shirt",
+    images: [
+      "/shirts/basic white/1.jpg",
+      "/shirts/basic white/2.jpg",
+      "/shirts/basic white/3.jpg"
+    ],
+    tags: ["shirt", "white"]
+  },
+  {
+    name: "white shirt boy",
+    images: [
+      "/shirts/basic white/1.jpg",
+      "/shirts/basic white/2.jpg",
+      "/shirts/basic white/3.jpg"
+    ],
+    tags: ["shirt", "white"]
+  },
+
+  /*********/
+  /* Pants */
+  /*********/
+  {
+    name: "gangster pants",
+    images: [
+      "/pants/gangster pants/1.jpg",
+      "/pants/gangster pants/2.jpg",
+      "/pants/gangster pants/3.jpg"
+    ],
+    tags: ["pants", "swaggy"]
+  },
+  {
+    name: "gangster pants",
+    images: [
+      "/pants/gangster pants/1.jpg",
+      "/pants/gangster pants/2.jpg",
+      "/pants/gangster pants/3.jpg"
+    ],
+    tags: ["pants", "swaggy"]
+  },
+  {
+    name: "swag",
+    images: [
+      "/pants/gangster pants/1.jpg",
+      "/pants/gangster pants/2.jpg",
+      "/pants/gangster pants/3.jpg"
+    ],
+    tags: ["pants", "swaggy"]
+  },
+
+  /***************/
+  /* Accessories */
+  /***************/
+  {
+    name: "matte black watch",
+    images: [
+      "/accessories/matte black watch/1.jpg",
+      "/accessories/matte black watch/2.jpg"
+    ],
+    tags: ["accessory", "watch", "black"]
+  },
+  {
+    name: "steel watch",
+    images: [
+      "/accessories/steel watch/1.jpg",
+      "/accessories/steel watch/2.jpg"
+    ],
+    tags: ["accessory", "watch", "black"]
+  },
+
+  /*********/
+  /* Shoes */
+  /*********/
+  {
+    name: "bitch mob 1s",
+    images: [
+      "/shoes/bitch mob 1s/1.jpg",
+      "/shoes/bitch mob 1s/2.jpg",
+      "/shoes/bitch mob 1s/3.jpg"
+    ],
+    tags: ["shoes", "camo"]
+  }
+];
+
+export default list;
