d900821cae7b2a02f32185f65dec2f75458fb0cd
Gage Henderson <gage@quangoinc.com>
2017-09-29 14:21:05 -0700

    Basic style setup, basic grid layout with flexbox

--- killstreak/src/components/Pages/Shop/Products/ProductItem.js
+++ killstreak/src/components/Pages/Shop/Products/ProductItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class ProductItem extends Component {
   render() {
-    return <div className="ProductItem">{this.props.name}</div>;
+    return <div className="ProductItem cell">{this.props.name}</div>;
   }
 }
 
