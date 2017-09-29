fe938e33089ed2530f6435ec57d0d057df443560
Gage Henderson <gage@quangoinc.com>
2017-09-29 13:15:04 -0700

    Added simple rotating 3d logo to shop page

--- killstreak/src/components/Pages/Shop/Header.js
+++ killstreak/src/components/Pages/Shop/Header.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import logo from "../../../images/logo/simple 3d red.png";
 
 class Header extends Component {
   render() {
-    return <div className="Header" />;
+    return (
+      <div className="Header">
+        {/* Logos */}
+        <div className="logo-container">
+          <img src={logo} alt="Killstreak" />
+          <img src={logo} alt="Killstreak" />
+          <img src={logo} alt="Killstreak" />
+          <img src={logo} alt="Killstreak" />
+        </div>
+      </div>
+    );
   }
 }
 
