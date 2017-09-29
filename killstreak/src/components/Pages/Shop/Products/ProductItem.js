b99ff4b942ca42246212d83b46e3b63338a7b3c3
Gage Henderson <gage@quangoinc.com>
2017-09-29 14:36:00 -0700

    Added some spacing between products

--- killstreak/src/components/Pages/Shop/Products/ProductItem.js
+++ killstreak/src/components/Pages/Shop/Products/ProductItem.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class ProductItem extends Component {
   render() {
-    return <div className="ProductItem cell">{this.props.name}</div>;
+    var thumbnail = require(`../../../../images/products${this.props
+      .images[0]}`);
+    return (
+      <div className="ProductItem cell">
+        <img src={thumbnail} className="thumbnail" alt="" />
+      </div>
+    );
   }
 }
 
