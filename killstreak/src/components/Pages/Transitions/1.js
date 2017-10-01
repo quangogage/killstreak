52eb4139301d469c4c95518c81acc100dbc8f7ff
quangogage <quangogage@users.noreply.github.com>
2017-09-30 23:51:56 -0700

    Disabled page scrolling during transitions

--- killstreak/src/components/Pages/Transitions/1.js
+++ killstreak/src/components/Pages/Transitions/1.js
@@ -1,166 +1,172 @@
 /* BANG BANG BRRRRRRRRRRRAAAAHAP */
-import $ from 'jquery';
-import gageMath from '../../../gagelib/Math';
+import $ from "jquery";
+import gageMath from "../../../gagelib/Math";
 
-import decalImage from '../../../images/transitions/shooting/decal.png';
-import logoImage from '../../../images/logo/white.png';
-
-
+import decalImage from "../../../images/transitions/shooting/decal.png";
+import logoImage from "../../../images/logo/white.png";
 
 // ** Animation Variables ** \\
-var duration=1750; // Length of the entire transition
+var duration = 1750; // Length of the entire transition
 
-var fadeInTime=300; // Milliseconds it takes to fade in initial overlay
-var fadeOutTime=300; // Same as above - But fading out
+var fadeInTime = 300; // Milliseconds it takes to fade in initial overlay
+var fadeOutTime = 300; // Same as above - But fading out
 
-var bulletAmount=50; // How many shots?
-var bulletRate=30; // Milliseconds between shots
-var bulletSpreadY=0.05 // Percent ( of screen ) of vertical spread
-var bulletSpreadX=0.6; // Percent ( of screen ) of horizontal spread
+var bulletAmount = 50; // How many shots?
+var bulletRate = 30; // Milliseconds between shots
+var bulletSpreadY = 0.05; // Percent ( of screen ) of vertical spread
+var bulletSpreadX = 0.6; // Percent ( of screen ) of horizontal spread
 
-var flashTime=15; // Fade out time in milliseconds
-var flashBrightness=0.5; // Brightness of flash (0-1)
-
-
+var flashTime = 15; // Fade out time in milliseconds
+var flashBrightness = 0.5; // Brightness of flash (0-1)
 
 // Create the red overlay element
 function createOverlay() {
-  var overlay=$(`<div class="overlay"></div>`);
+  var overlay = $(`<div class="overlay"></div>`);
   overlay.css({
-    position:'absolute',
-    left:0,
-    top:'-100%',
-    width:'100%',
-    height:'100%',
-    zIndex:10,
-    background:'RGB(255,0,0)',
-    opacity:0,
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-  })
-  var logo=$(`<img src=${logoImage} />`);
+    position: "absolute",
+    left: 0,
+    top: "-100%",
+    width: "100%",
+    height: "100%",
+    zIndex: 10,
+    background: "RGB(255,0,0)",
+    opacity: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  });
+  var logo = $(`<img src=${logoImage} />`);
   logo.css({
-    zIndex:2,
-    width:'80%'
-  })
+    zIndex: 2,
+    width: "80%"
+  });
   overlay.append(logo);
-  
+
   return overlay;
 }
 
 // Create or update a flash effect when "shoot"ing
 function flash(overlay) {
   var flash;
-  // Create flash if it doesn't already exist  
-  if ($('.overlay .flash').length===0) {
-    flash=$('<div class="flash"></div>');
+  // Create flash if it doesn't already exist
+  if ($(".overlay .flash").length === 0) {
+    flash = $('<div class="flash"></div>');
     flash.css({
-      position:'absolute',
-      left:0,
-      top:0,
-      width:'100%',
-      height:'100%',
-      background:'white',
-      zIndex:5,
-    })
+      position: "absolute",
+      left: 0,
+      top: 0,
+      width: "100%",
+      height: "100%",
+      background: "white",
+      zIndex: 5
+    });
     overlay.append(flash);
   } else {
-    flash=$('.overlay .flash');
+    flash = $(".overlay .flash");
   }
-  flash.css({opacity:flashBrightness});
-  flash.animate({
-    opacity:0,
-  },{duration:flashTime,queue:false})
-  
+  flash.css({ opacity: flashBrightness });
+  flash.animate(
+    {
+      opacity: 0
+    },
+    { duration: flashTime, queue: false }
+  );
 }
 
 // Add a decal
-function createDecal(x,overlay) {
-  var decal=$(`<img class='decal' src=${decalImage} />`);
-  var y=gageMath.getRandom(window.innerHeight*(0.5-bulletSpreadY),window.innerHeight*(0.5+bulletSpreadY));
-  var size=gageMath.getRandom(15,25);
-  var rotation=gageMath.getRandom(0,360);
+function createDecal(x, overlay) {
+  var decal = $(`<img class='decal' src=${decalImage} />`);
+  var y = gageMath.getRandom(
+    window.innerHeight * (0.5 - bulletSpreadY),
+    window.innerHeight * (0.5 + bulletSpreadY)
+  );
+  var size = gageMath.getRandom(15, 25);
+  var rotation = gageMath.getRandom(0, 360);
 
   // Position and style
   decal.css({
-    position:'absolute',
-    left:x+'px',
-    top:y+'px',
-    width:size+'px',
-    height:size+'px',
-    zIndex:3,
-    transform:`rotate(${rotation}deg)`
-  })
+    position: "absolute",
+    left: x + "px",
+    top: y + "px",
+    width: size + "px",
+    height: size + "px",
+    zIndex: 3,
+    transform: `rotate(${rotation}deg)`
+  });
 
   // Add to overlay
-  overlay.append(decal);  
+  overlay.append(decal);
 }
 
 // Begin shooting
 function beginShooting(overlay) {
-  var x=(window.innerWidth/2)-((bulletSpreadX*window.innerWidth)/2);
-  var xInc=((bulletSpreadX*window.innerWidth)/bulletAmount);
+  var x = window.innerWidth / 2 - bulletSpreadX * window.innerWidth / 2;
+  var xInc = bulletSpreadX * window.innerWidth / bulletAmount;
 
-  var shooting=setInterval(function() {
-
+  var shooting = setInterval(function() {
     // Shoot
-    createDecal(x,overlay);
+    createDecal(x, overlay);
     flash(overlay);
 
     // Push next one to the right
-    x+=xInc;    
+    x += xInc;
 
     // Stop shooting
-    if ($(".overlay .decal").length>=bulletAmount) {
+    if ($(".overlay .decal").length >= bulletAmount) {
       clearInterval(shooting);
     }
-  },bulletRate)    
+  }, bulletRate);
 }
-
 
 // Create container for the entire transition
 function createContainer() {
-  var container=$("<div class='transition'></div>");
-  $('body').append(container);
+  var container = $("<div class='transition'></div>");
+  $("body").append(container);
   return container;
 }
 
 // Master Function \\
-function animation(setPage,newPage) {
-  var container=createContainer();  
-  var overlay=createOverlay();
+function animation(setPage, newPage) {
+  var container = createContainer();
+  var overlay = createOverlay();
   container.append(overlay);
 
   // No scroll body
-  $('body').css({overflow:'hidden'});
+  $(".page").addClass("noscroll");
 
   // Fade overlay in
-  overlay.animate({
-    opacity:1,
-    top:0
-  },fadeInTime, function() {
+  overlay.animate(
+    {
+      opacity: 1,
+      top: 0
+    },
+    fadeInTime,
+    function() {
+      // Change pages while they can't see it!
+      setPage(newPage);
 
-    // Change pages while they can't see it!
-    setPage(newPage);
+      // BRRRrrrrRRrrrrrrrrrrrRRRRRAAAAAP🔫
+      setTimeout(function() {
+        beginShooting(overlay);
 
-    // BRRRrrrrRRrrrrrrrrrrrRRRRRAAAAAP🔫
-    setTimeout(function() {    
-      beginShooting(overlay);
-
-      // Fade out and clear out!
-      setTimeout(function() {        
-        overlay.animate({
-          opacity:0,
-          top:'100%'
-        },fadeOutTime, function() {
-          container.empty();
-          container.remove();          
-          $('body').css({overflow:''});
-        })        
-      },duration)
-    },400)
-  });
+        // Fade out and clear out!
+        setTimeout(function() {
+          overlay.animate(
+            {
+              opacity: 0,
+              top: "100%"
+            },
+            fadeOutTime,
+            function() {
+              container.empty();
+              container.remove();
+              $(".page").removeClass("noscroll");
+            }
+          );
+        }, duration);
+      }, 400);
+    }
+  );
 }
 
-export default { animation }
+export default { animation };
--- killstreak/src/components/Pages/Transitions/2.js
+++ killstreak/src/components/Pages/Transitions/2.js
@@ -1,115 +1,125 @@
 /* ...sssshhhhhhhHHHHIIIIIING */
-import $ from 'jquery';
-import gageMath from '../../../gagelib/Math';
-import '../../../styles/css/Transitions/2.css';
-import mojs from 'mo-js';
+import $ from "jquery";
+import gageMath from "../../../gagelib/Math";
+import "../../../styles/css/Transitions/2.css";
+import mojs from "mo-js";
 
-import logoImage from '../../../images/logo/white.png';
-
+import logoImage from "../../../images/logo/white.png";
 
 // ** Animation Variables ** \\
-var duration=2500; // Total length of transition ( milliseconds )
+var duration = 2500; // Total length of transition ( milliseconds )
 
-var sliceDuration=1000; // How long does the slice take?
+var sliceDuration = 1000; // How long does the slice take?
 
-var flashDelay=875; // How long after slice should it flash?
-var flashDuration=500; // How long does the flash last?
-
-
+var flashDelay = 875; // How long after slice should it flash?
+var flashDuration = 500; // How long does the flash last?
 
 // Create container for entire transition
 function createContainer() {
-  var container=$("<div class='transition'></div>");
-  $('body').append(container);
+  var container = $("<div class='transition'></div>");
+  $("body").append(container);
   return container;
 }
 
 // Create the overlay
 function createOverlay() {
-  var overlay=$('<div class="overlay-2" id="transition"></div>');
-  var left=$(`<div class="side left"><img src=${logoImage} /></div>`);
-  var right=$(`<div class="side right"><img src=${logoImage} /></div>`);
+  var overlay = $('<div class="overlay-2" id="transition"></div>');
+  var left = $(`<div class="side left"><img src=${logoImage} /></div>`);
+  var right = $(`<div class="side right"><img src=${logoImage} /></div>`);
 
   overlay.append(left, right);
 
   return overlay;
 }
 
-
 // SHINK! ⚔️
 function createSlice() {
-  const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
-  const scaleCurveBase = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,-700 C50,80.1708527 76.6222458,93.9449005 100,100' );
-  const scaleCurve = (p) => { return 1 + scaleCurveBase(p); };
-  const nScaleCurve = (p) => { return 1 - scaleCurveBase(p)/10; };
-  var screenWidth=window.innerWidth;
-  var screenHeight=window.innerHeight;
-  var angle=gageMath.toDegrees(Math.atan2((window.innerWidth-0),(window.innerHeight-0))-Math.PI/2);
+  const shiftCurve = mojs.easing.path(
+    "M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0"
+  );
+  const scaleCurveBase = mojs.easing.path(
+    "M0,100 C21.3776817,95.8051376 50,77.3262711 50,-700 C50,80.1708527 76.6222458,93.9449005 100,100"
+  );
+  const scaleCurve = p => {
+    return 1 + scaleCurveBase(p);
+  };
+  const nScaleCurve = p => {
+    return 1 - scaleCurveBase(p) / 10;
+  };
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+  var angle = gageMath.toDegrees(
+    Math.atan2(window.innerWidth - 0, window.innerHeight - 0) - Math.PI / 2
+  );
   const circle = new mojs.Shape({
-    parent:        '#transition',
-    shape:        'circle',
-    fill:         { '#FFFFFF' : '#FFFFFF', curve: scaleCurve },
-    radius:       10,
-    rx:           3,
-    x:            { [screenWidth/2] : -screenWidth/2, easing: shiftCurve},
-    y:            { [-screenHeight/2]: screenHeight/2, easing : shiftCurve},
-    scaleX:       { 10 : 10, curve: scaleCurve },
-    scaleY:       { 1 : 1, curve: nScaleCurve },
-    origin:       { '0 50%' : '100% 50%', easing: shiftCurve },
+    parent: "#transition",
+    shape: "circle",
+    fill: { "#FFFFFF": "#FFFFFF", curve: scaleCurve },
+    radius: 10,
+    rx: 3,
+    x: { [screenWidth / 2]: -screenWidth / 2, easing: shiftCurve },
+    y: { [-screenHeight / 2]: screenHeight / 2, easing: shiftCurve },
+    scaleX: { 10: 10, curve: scaleCurve },
+    scaleY: { 1: 1, curve: nScaleCurve },
+    origin: { "0 50%": "100% 50%", easing: shiftCurve },
     angle: angle,
-    
-    isYoyo:       false,
-    delay:        500,
-    duration:     sliceDuration,
-    repeat:       999,
+
+    isYoyo: false,
+    delay: 500,
+    duration: sliceDuration,
+    repeat: 999
   }).play();
-  circle.el.style.zIndex='6';
+  circle.el.style.zIndex = "6";
 }
 
 // 📸
 function createFlash(overlay) {
-  var flash=$(`<div class='flash'></div>`);
-  var delay=flashDelay/1000+'s';
-  var duration=flashDuration/1000+'s';
+  var flash = $(`<div class='flash'></div>`);
+  var delay = flashDelay / 1000 + "s";
+  var duration = flashDuration / 1000 + "s";
 
   flash.css({
-    animationDelay:delay,
-    animationDuration:duration
-  })
+    animationDelay: delay,
+    animationDuration: duration
+  });
 
   overlay.append(flash);
 }
 
+function animation(setPage, newPage) {
+  var container = createContainer();
+  var overlay = createOverlay();
 
-function animation(setPage,newPage) { 
-  var container=createContainer(); 
-  var overlay=createOverlay();
-
+  // Place the overlay onto the transition container
   container.append(overlay);
+
+  // Disable page scrolling
+  $(".page").addClass("noscroll");
 
   setTimeout(function() {
     createSlice();
     createFlash(overlay);
 
     setTimeout(function() {
-      var sides=$('.side');
+      var sides = $(".side");
 
       // Move the sides apart
-      sides.addClass('sliced');        
+      sides.addClass("sliced");
 
       // Change page
-      setPage(newPage);      
-
-    },flashDelay+(flashDuration/2))
-  },500);
-
+      setPage(newPage);
+    }, flashDelay + flashDuration / 2);
+  }, 500);
 
   // CUT IT!
-  setTimeout(function(){
+  setTimeout(function() {
     container.empty();
     container.remove();
     setPage(newPage);
-  },duration);
+
+    // Re-allow scrolling
+    $(".page").removeClass("noscroll");
+  }, duration);
 }
 
-export default { animation }
+export default { animation };
