d035b7dbf01522b8380d20d6e8fe0bb92917ef04
quangogage <quangogage@users.noreply.github.com>
2017-09-30 23:43:47 -0700

    Added some basic descriptions n shit

--- killstreak/src/components/Pages/PageHandler.js
+++ killstreak/src/components/Pages/PageHandler.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react'
-import $ from 'jquery';
-import gageMath from '../../gagelib/Math';
+import React, { Component } from "react";
+import $ from "jquery";
+import gageMath from "../../gagelib/Math";
 
 /* Pages */
-import Home from './Home/Home';
-import About from './About/About';
-import Contact from './Contact/Contact';
-import Shop from './Shop/Shop';
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Shop from "./Shop/Shop";
 
-var transitionCount=2; // Update this whenever you add a new animation to `./Transitions`
+var transitionCount = 2; // Update this whenever you add a new animation to `./Transitions`
+
+/*
+  This component basically acts as a custom
+  routing system. 
+  
+  The current URL gets passed down to it from
+  `../index.js` as `location.pathname` - It uses
+  this prop to check if the user has just entered,
+  or switched URLS, and deals with that information
+  accordingly.
+
+
+  TRANSITION INFO: These are the animations that play
+  when you switch pages. They are stored in the 
+  `./Transitions/` directory,
+
+  CREATING A TRANSITION:
+    - Make a new file in the directory with the 
+      appropriate numerical name.
+
+    - Create a stylesheet for the transition and
+      import it into your new file ( I did not do
+      this for the first transition and it sucked) 
+
+    - Create an `animation` function with `setPage`
+      and `newPage` as the first and second arguments.
+
+    - Make your animation inside of that function!
+
+      • To load the next page use `setPage(newPage)`.
+        Try to do so when the entire screen is covered.
+        
+        
+*/
 
 class PageHandler extends Component {
-
   constructor(props) {
     super(props);
-    this.state={      
-      currentPage:"Home",
-      pages:[
-        "Home",  
-        "Shop",
-        "About",
-        "Contact"
-      ]
-    }  
-    
-    this.setPage=this.setPage.bind(this);
+    this.state = {
+      currentPage: "Home",
+      pages: ["Home", "Shop", "About", "Contact"]
+    };
+
+    this.setPage = this.setPage.bind(this);
   }
 
   // Set initial page. This is for if you
@@ -33,13 +61,11 @@ class PageHandler extends Component {
   // using the Links within.
   componentDidMount() {
     this.setState({
-      currentPage:this.props.location.pathname.substr(1)
-    })
+      currentPage: this.props.location.pathname.substr(1)
+    });
   }
 
-
   componentWillReceiveProps(nextProps) {
-    
     // Check for URL change
     if (nextProps.location.pathname !== this.props.location.pathname) {
       this.initTransition(nextProps.location.pathname.substr(1));
@@ -48,48 +74,37 @@ class PageHandler extends Component {
 
   // Initialize page transition animation
   initTransition(page) {
-    var animIndex=Math.floor(gageMath.getRandom(1,transitionCount));
-    var anim=require(`./Transitions/${animIndex}.js`);
+    var animIndex = Math.floor(gageMath.getRandom(1, transitionCount));
+    var anim = require(`./Transitions/${animIndex}.js`);
 
     // Check if one is already running ( if so, stahp! )
-    if ($('.transition').length!==0) {
-      $('.transition').empty();
-      $('.transition').remove();
+    if ($(".transition").length !== 0) {
+      $(".transition").empty();
+      $(".transition").remove();
     }
 
     // Run it!
-    anim.default.animation(this.setPage,page);
+    anim.default.animation(this.setPage, page);
   }
 
   // Set the current page (used within transitions)
   setPage(page) {
     this.setState({
-      currentPage:page
-    })
+      currentPage: page
+    });
   }
-
 
   // required for react Component class extension
   render() {
     return (
-      <div className="PageHandler">        
-
-        <Home
-          currentPage={this.state.currentPage}
-          pages={this.state.pages}
-        />        
-        <About
-          currentPage={this.state.currentPage}
-          pages={this.state.pages}
-        />
+      <div className="PageHandler">
+        <Home currentPage={this.state.currentPage} pages={this.state.pages} />
+        <About currentPage={this.state.currentPage} pages={this.state.pages} />
         <Contact
           currentPage={this.state.currentPage}
           pages={this.state.pages}
-        />         
-        <Shop
-          currentPage={this.state.currentPage}
-          pages={this.state.pages}
-        />         
+        />
+        <Shop currentPage={this.state.currentPage} pages={this.state.pages} />
       </div>
     );
   }
