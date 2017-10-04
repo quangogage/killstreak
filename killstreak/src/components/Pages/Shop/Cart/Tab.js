cb85619b3c5470b65d9708019e32d9648e5ceffd
Gage Henderson <gage@quangoinc.com>
2017-10-04 14:39:37 -0700

    Added shopping cart icon

--- killstreak/src/components/Pages/Shop/Cart/Tab.js
+++ killstreak/src/components/Pages/Shop/Cart/Tab.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import cartIcon from "../../../../images/icons/shopping-cart.png";
+
 class Tab extends Component {
   render() {
     return <div className="Tab" />;
