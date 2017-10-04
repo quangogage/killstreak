14d4e779557f2266729e74310732fb23d910d8af
Gage Henderson <gage@quangoinc.com>
2017-10-04 14:40:34 -0700

    Now rendering cart icon and cart amount

--- killstreak/src/components/Pages/Shop/Cart/Tab.js
+++ killstreak/src/components/Pages/Shop/Cart/Tab.js
@@ -4,7 +4,12 @@ import cartIcon from "../../../../images/icons/shopping-cart.png";
 
 class Tab extends Component {
   render() {
-    return <div className="Tab" />;
+    return (
+      <div className="Tab">
+        <img src={cartIcon} className="icon" alt="" />
+        <div className="amount">({this.props.amount})</div>
+      </div>
+    );
   }
 }
 
