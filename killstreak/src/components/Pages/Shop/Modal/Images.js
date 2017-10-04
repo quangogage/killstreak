70d3767ffa049cb5397cd822b7b9f9e0fb5652bd
Gage Henderson <gage@quangoinc.com>
2017-10-04 09:57:01 -0700

    Displayed 'selected' image in Images component, basic styles

--- killstreak/src/components/Pages/Shop/Modal/Images.js
+++ killstreak/src/components/Pages/Shop/Modal/Images.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
 class Images extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 0
+    };
+  }
   render() {
-    return <div className="Images section" />;
+    var selectedPath = this.props.list[this.state.selected];
+    var selectedImage = require("../../../../images/products" + selectedPath);
+    return (
+      <div className="Images section">
+        <img src={selectedImage} className="selected" alt="" />
+      </div>
+    );
   }
 }
 
