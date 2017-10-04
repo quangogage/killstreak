04813090723069bd52c0ff368cd5c9c3921f183c
Gage Henderson <gage@quangoinc.com>
2017-10-04 14:30:23 -0700

    Added cart tab component

--- killstreak/src/components/Pages/Shop/Cart/Cart.js
+++ killstreak/src/components/Pages/Shop/Cart/Cart.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Tab from "./Tab";
 
 class Cart extends Component {
   render() {
-    return <div className="Cart" />;
+    return (
+      <div className="Cart">
+        {/* Cart Tab */}
+        <Tab amount={this.props.items.length} />
+      </div>
+    );
   }
 }
 
---
+++ killstreak/src/components/Pages/Shop/Cart/Tab.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Tab extends Component {
+  render() {
+    return <div className="Tab" />;
+  }
+}
+
+export default Tab;
