38fa58ef6e89a197da6ea04acce9107429cf3026
Gage Henderson <gage@quangoinc.com>
2017-10-04 17:22:22 -0700

    Car tab now automatically moves down if overlapping with navigation menu

--- killstreak/src/components/Pages/Shop/Cart/Tab.js
+++ killstreak/src/components/Pages/Shop/Cart/Tab.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 import cartIcon from "../../../../images/icons/shopping-cart.png";
 
 class Tab extends Component {
+  componentDidMount() {
+    this.checkTab();
+    $(window).scroll(
+      function(e) {
+        this.checkTab();
+      }.bind(this)
+    );
+  }
+
+  // Check and adjust if tab is overlapping with navigation menu
+  checkTab() {
+    var tab = $(".Cart .Tab");
+    var nav = $(".Page-Nav");
+    if (window.scrollY + tab.offset().top <= nav.height() * 2) {
+      tab.css({
+        top: nav.height() - window.scrollY + "px"
+      });
+    } else {
+      tab.css({
+        top: ""
+      });
+    }
+  }
   render() {
     return (
       <div className="Tab">
