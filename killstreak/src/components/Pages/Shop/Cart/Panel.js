05c4e7d963f77e4d486f6c2de0f8fec04301048e
Gage Henderson <gage@quangoinc.com>
2017-10-05 14:27:29 -0700

    added fresh panel component and stylesheet

---
+++ killstreak/src/components/Pages/Shop/Cart/Panel.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../../../../styles/css/Shop/Panel.css";
+
+class Panel extends Component {
+  render() {
+    return <div className="Panel" />;
+  }
+}
+
+export default Panel;
