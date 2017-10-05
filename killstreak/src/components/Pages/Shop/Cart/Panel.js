f2e6f79b38b56348c4fe7fce7fc567cd1b58c143
Gage Henderson <gage@quangoinc.com>
2017-10-05 14:58:29 -0700

    More progress on new panel. Closed state pretty much complete, going to move onto opening

--- killstreak/src/components/Pages/Shop/Cart/Panel.js
+++ killstreak/src/components/Pages/Shop/Cart/Panel.js
@@ -22,16 +22,23 @@ class Panel extends Component {
   }
 
   // Open up!
-  open() {}
+  open() {
+    alert("opening");
+  }
 
   // Shut 'er down.
-  close() {}
+  close() {
+    alert("closing");
+  }
 
   render() {
     return (
-      <div className="Panel" onClick={this.props.open}>
-        <div className="closed-container">
+      <div className={"Panel" + (this.props.isOpen ? " open" : " closed")}>
+        {/* Content that appears when panel is closed */}
+        <div className="closed-container" onClick={this.props.open}>
+          <div className="text">View Cart</div>
           <img src={cartIcon} className="icon" />
+          <div className="count">({this.props.itemCount})</div>
         </div>
       </div>
     );
